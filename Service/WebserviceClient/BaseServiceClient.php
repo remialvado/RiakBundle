033c8b6c27dc3276ffbe3fc6b0e853e6b1c8f228
Rémi <remi.alvado+git@gmail.com>
2013-04-05 22:51:17 +0200

    remove dead code about post requests as it is not necessary anymore : guzzle does not seem to cause performance issues anymore

--- Service/WebserviceClient/BaseServiceClient.php
+++ Service/WebserviceClient/BaseServiceClient.php
@@ -13,28 +13,6 @@ abstract class BaseServiceClient
         return $guzzleClientProvider->getClient($this->route, $config);
     }
 
-    /**
-     * Post requests are really slow using Guzzle. I don't know why but using curl
-     * directly is the only solution we found so far.
-     * @return \Guzzle\Http\Message\Response
-     */
-    public function executePostRequest($url, $data, $headers = null)
-    {
-        $ch = curl_init();
-        $this->logger->debug("[POST] $url");
-        curl_setopt($ch, CURLOPT_URL,$url);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
-        if (is_array($headers)) curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        $result = curl_exec($ch);
-        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
-
-        return new Response($httpCode, null, $result);
-    }
-
     public function getRoute()
     {
         return $this->route;
